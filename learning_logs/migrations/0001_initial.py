b9b6ee2cb512c00d93bd36e432534fff5327567c
= <=>
2018-04-14 22:44:02 +0800

    S

---
+++ learning_logs/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Topic',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('text', models.CharField(max_length=200)),
+                ('date_added', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+    ]
