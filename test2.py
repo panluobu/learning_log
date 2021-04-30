a148439b17e80febf26890accf2af6b79bef2c11
DESKTOP-9AQ77AQ\31006 <panluobu@gmail.com>
2021-05-01 01:52:36 +0800

    test2&3

---
+++ test2.py
@@ -0,0 +1,30 @@
+#3-1
+names = ['xigua', 'mihoutao', 'mangguo', 'lajiao', 'huluobo', 'niuyouguo']
+print(names[0], names[1], names[2], names[3], names[4], names[5])
+#3-2
+print("Hello "+names[0])
+#3-3
+
+#3-4
+name = "zeng yin"
+print(name.lower())
+print(name.upper())
+print(name.title())
+#3-5&2-6
+name = 'Albert Einstein'
+word = ' once said, "A person who never made a mistake never tried anything new."'
+print(name+word)
+#3-7
+name = "\nzengyin\t"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+#3-8
+print(2+6)
+print(9-1)
+print(1*8)
+print(64//8)
+#3-9
+number = 9
+print("我最喜欢的数字是"+str(number))
