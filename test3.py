f2430c3f554a134b22b93e6fb3ba7713634a95f2
DESKTOP-9AQ77AQ\31006 <panluobu@gmail.com>
2021-05-01 01:54:21 +0800

    test3

---
+++ test3.py
@@ -0,0 +1,32 @@
+#2-1
+message = "Hello world!"
+print(message)
+#2-2
+message = "Hello new world!"
+print(message)
+#2-3
+name = "Eric"
+print("Hello "+name+", would you like to learn some Python today?")
+#2-4
+name = "zeng yin"
+print(name.lower())
+print(name.upper())
+print(name.title())
+#2-5&2-6
+name = 'Albert Einstein'
+word = ' once said, "A person who never made a mistake never tried anything new."'
+print(name+word)
+# 2-7
+name = "\nzengyin\t"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+# 2-8
+print(2+6)
+print(9-1)
+print(1*8)
+print(64//8)
+# 2-9
+number = 9
+print("我最喜欢的数字是"+str(number))
